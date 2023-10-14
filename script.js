2471e345f0b05a72a8987a7b132b04e9480b8c4a
uradjudicator <prind0241@gmail.com>
2023-10-14 13:32:13 +0500

    init:hover burger

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.bx-menu')
+const sideBar = document.querySelector('.sidebar')
+
+menu.addEventListener('mousemove', () =>{
+    sideBar.classList.toggle('close')
+})
+
+
+
+
